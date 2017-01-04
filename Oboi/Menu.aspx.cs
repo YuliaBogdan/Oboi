d05a5311a6bd2422f78635fbcbeae977863aa872
Yulia Bogdan <yuliabogdan666@gmail.com>
2017-01-04 13:29:09 +0300

    Wallpapers

---
+++ Oboi/Menu.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Oboi
+{
+    public partial class Menu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        protected void Find_Click(object sender, EventArgs e)
+        {
+            string x = Article_DB.Text;
+            Wallpaper.ImageUrl = "Resources/"+ x +".jpg";
+
+           
+        }
+     }
+
+}
+    
